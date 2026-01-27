8be483b2d8d24cb7e86d53b6395f8b77c40a1c49
Erika Buckley <96355027+Erikabuckley@users.noreply.github.com>
2026-01-27 17:18:17 +0000

    added subission screen

--- util.js
+++ util.js
@@ -1,3 +1,14 @@
 document.getElementById("logo").onclick = function () {
     location.href = "https://erikabuckley.github.io/GroupProject/index.html";
 };
+
+var modal = document.getElementById("upload-modal");
+var btn = document.getElementById("upload");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+};
+span.onclick = function() {
+  modal.style.display = "none";
+}
