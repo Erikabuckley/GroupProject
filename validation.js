eabc93f5b1ab2a252ec971d3803a6627708270d8
Erika Buckley <96355027+Erikabuckley@users.noreply.github.com>
2026-01-27 21:57:07 +0000

    improved validation

--- validation.js
+++ validation.js
@@ -1,6 +1,13 @@
-form.addEventListener('submit', (e) => {
+const form = document.getElementById('form')
+
+form.addEventListener('signin', (e) => {
     e.preventDefault(); // stop page reload
     window.location.href="https://erikabuckley.github.io/GroupProject/dashboard.html";
+});
+
+form.addEventListener('signup', (e) => {
+    e.preventDefault(); // stop page reload
+    window.location.href="https://erikabuckley.github.io/GroupProject/login.html";
 });
 
 form.addEventListener('confirm', (e) => {
