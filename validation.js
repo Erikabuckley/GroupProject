6304726f9b7cc797a1d985ede58ce7d0a368ad51
Erika Buckley <96355027+Erikabuckley@users.noreply.github.com>
2026-01-27 22:12:28 +0000

    updated for consistency

--- validation.js
+++ validation.js
@@ -3,16 +3,12 @@ const form = document.getElementById('form')
 form.addEventListener('submit', (e) => {
     e.preventDefault(); // stop page reload
     const action = e.submitter.value;
-    if (action === 'signin') {
+    if (action === 'Sign in') {
         window.location.href = "https://erikabuckley.github.io/GroupProject/dashboard.html";
-     } else if (action === 'signup') {
+     } else if (action === 'Sign up') {
         window.location.href = "https://erikabuckley.github.io/GroupProject/login.html";
+    }  else if (action === 'Confirm') {
+        window.location.href = "https://erikabuckley.github.io/GroupProject/dashboard.html";
     }
 });
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault(); // stop page reload
-    if (action === 'confirm') {
-        window.location.href="https://erikabuckley.github.io/GroupProject/dashboard.html";
-    }
-});
