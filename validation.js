4593143934cfb520e5b3f0dff3bdfd26a1cdd956
Erika Buckley <96355027+Erikabuckley@users.noreply.github.com>
2026-01-30 20:25:54 +0000

    updated for constiency

--- validation.js
+++ validation.js
@@ -4,8 +4,8 @@ form.addEventListener('submit', async (e) => {
     e.preventDefault(); // stop page reload
     const action = e.submitter.value;
     if (action === 'Sign in') {
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
+        const email = document.getElementById("email-input").value;
+        const password = document.getElementById("password-input").value;
         await fetch("https://groupproject-e980.onrender.com/login",
             {
                 method: "POST",
@@ -18,13 +18,13 @@ form.addEventListener('submit', async (e) => {
         );
         window.location.href = "https://erikabuckley.github.io/GroupProject/dashboard.html";
         } 
-        else if (action === 'Sign up') {
-            const email = document.getElementById("email").value;
-            const password = document.getElementById("password").value;
-            const name = document.getElementById("name").value;
-            const priv = document.getElementById("priv").value;
-            const tandc = document.getElementById("tandc").value;
-                await fetch("https://groupproject-e980.onrender.com/signUp",
+    else if (action === 'Sign up') {
+        const email = document.getElementById("email-input").value;
+        const password = document.getElementById("password-input").value;
+        const name = document.getElementById("name-input").value;
+        const priv = document.getElementById("priv").value;
+        const tandc = document.getElementById("tandc").value;
+        await fetch("https://groupproject-e980.onrender.com/signUp",
             {
                 method: "POST",
                 headers: {
