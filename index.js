f6c164adc1d3b1144a6238995ea7a4a1ac44bed9
Erika Buckley <96355027+Erikabuckley@users.noreply.github.com>
2026-01-30 20:11:09 +0000

    added jjs for login

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.use(express.json());
+
+// get data from the login
+app.post('/login', function (req, res) {
+    console.log("Login request received");
+    console.log(req.body.name);
+    res.end();
+});
+
 // Define a route for GET requests to the root URL
 app.get('/', (req, res) => {
   res.send('Hello World from Express!');
--- validation.js
+++ validation.js
@@ -1,11 +1,37 @@
 const form = document.getElementById('form')
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
     e.preventDefault(); // stop page reload
     const action = e.submitter.value;
     if (action === 'Sign in') {
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        await fetch("https://groupproject-e980.onrender.com/login",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type" : "application/x-www-form-urlencoded"
+                },
+                body : JSON.stringify({email, password}   
+                )
+            }
+        );
         window.location.href = "https://erikabuckley.github.io/GroupProject/dashboard.html";
-     } else if (action === 'Sign up') {
+        } 
+        else if (action === 'Sign up') {
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+            const name = document.getElementById("name").value;
+                await fetch("https://groupproject-e980.onrender.com/signUp",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type" : "application/x-www-form-urlencoded"
+                },
+                body : JSON.stringify({email, password,name}   
+                )
+            }
+        );
         window.location.href = "https://erikabuckley.github.io/GroupProject/login.html";
     }  else if (action === 'Confirm') {
         window.location.href = "https://erikabuckley.github.io/GroupProject/dashboard.html";
