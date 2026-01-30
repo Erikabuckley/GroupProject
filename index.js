9d659b6b4d9392ed6b225c3101a8c06d02a88788
Erika Buckley <96355027+Erikabuckley@users.noreply.github.com>
2026-01-30 20:34:43 +0000

    allowed our host web app

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const cors = require("cors");
 const app = express();
 const port = 8080;
 
 app.use(express.json());
+app.use(cors({ 
+  origin: "https://erikabuckley.github.io",
+  methods: ["GET", "POST"],
+ }));
 
 // get data from the login
 app.post('/login', function (req, res) {
