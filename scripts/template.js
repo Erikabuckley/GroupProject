2ee2bc95d75eae469a57568002707875138274d8
dal209 <dal209@exeter.ac.uk>
2026-02-06 19:45:47 +0000

    template change

--- scripts/template.js
+++ scripts/template.js
@@ -1,5 +1,5 @@
 async function loadFooter(){
-    const res = await fetch("../../templates/footer.html");
+    const res = await fetch("../templates/footer.html");
     const html = await res.text();
     document.getElementsByClassName("footer-container")[0].innerHTML = html;
 };
@@ -37,7 +37,7 @@ async function loadHeader() {
                 };
             }else{
                 exit.onclick = function () {
-                    window.location.href = "../../dash/dashboard.html";
+                    window.location.href = "../dash/dashboard.html";
                 };
             }
         }
@@ -45,27 +45,27 @@ async function loadHeader() {
 
     if (document.URL.includes('dash')){
         document.getElementById("logo").onclick = function () {
-        window.location.href = "../../dash/dashboard.html"};
+        window.location.href = "../dash/dashboard.html"};
     }else {
         document.getElementById("logo").onclick = function () {
-        window.location.href = "../../index.html";}
+        window.location.href = "../index.html";}
     };
 }
 
 async function loadBasicHeader(){
-    const res = await fetch("../../templates/basicHeader.html")
+    const res = await fetch("../templates/basicHeader.html")
     const html = await res.text();
     document.getElementsByClassName("header-container")[0].innerHTML = html;
 }
 
 async function loadHomeHeader(){
-    const res = await fetch("../../templates/homeHeader.html")
+    const res = await fetch("../templates/homeHeader.html")
     const html = await res.text();
     document.getElementsByClassName("header-container")[0].innerHTML = html;
 }
 
 async function loadDashHeader(){
-    const res = await fetch("../../templates/dashHeader.html")
+    const res = await fetch("../templates/dashHeader.html")
     const html = await res.text();
     document.getElementsByClassName("header-container")[0].innerHTML = html;
 }
