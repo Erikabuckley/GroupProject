4458390d8076ff320e5793205105274502ffd6e9
Erika Buckley <96355027+Erikabuckley@users.noreply.github.com>
2026-03-25 22:34:23 +0000

    Enhance comments in generateSitemap function
    Added comments for clarity in the sitemap generation process.

--- sitemap.js
+++ sitemap.js
@@ -3,13 +3,14 @@ import path from "path";
 
 export function generateSitemap() {
 
+    // Establish a base url for all pages
     const baseUrl = "https://groupproject-e980.onrender.com/";
     const publicDir = path.join(process.cwd(), "public");
     const output = path.join(publicDir, "sitemap.xml");
 
     function getHtmlFiles(dir, list = []) {
         const files = fs.readdirSync(dir);
-
+        // Go through all files in the system
         for (const file of files) {
 
             const full = path.join(dir, file);
@@ -18,7 +19,7 @@ export function generateSitemap() {
             if (stat.isDirectory()) {
                 getHtmlFiles(full, list);
             }
-
+            // If it is an html file then add it to site map
             else if (file.endsWith(".html")) {
                 list.push(full);
             }
@@ -34,7 +35,7 @@ export function generateSitemap() {
         const relative = path
             .relative(publicDir, file)
             .replace(/\\/g, "/");
-
+        // Concatinate base url with file
         const loc = `${baseUrl}/${relative}`;
 
         const lastmod = new Date().toISOString().split("T")[0];
@@ -46,7 +47,7 @@ export function generateSitemap() {
   </url>`;
 
     }).join("");
-
+    // Format site map propely to be displayed using xml
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls}
